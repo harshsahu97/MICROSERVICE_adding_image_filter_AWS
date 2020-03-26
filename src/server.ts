946001fa90e7b4368193aaa701a489c7a3e54218
Harsh Sahu <harsh.sahu@outlook.com>
2020-03-27 03:14:45 +0530

    UDagram

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+
+(async () => {
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get("/filteredimage", async (req, res) => {
+    let image_url = req.query.image_url;
+    if (image_url) {
+      filterImageFromURL(image_url).then(response => {
+        res.sendFile(response);
+        res.on("finish", function() {
+          deleteLocalFiles([response]);
+        });
+      });
+    } else {
+      res.status(422).send("Invalid URL");
+    }
+  });
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
